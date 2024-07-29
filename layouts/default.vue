73a22d9d0e3919e5ab877eec60fe1c0529f1febe
Melody Wu <110306066@g.nccu.edu.tw>
2024-07-29 22:28:16 +0800

    install tailwind, prettier

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,42 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <el-menu
+      default-active="2"
+      class="h-full w-[200px]"
+      @open="handleOpen"
+      @close="handleClose"
+    >
+      <el-sub-menu index="1">
+        <template #title>
+          <el-icon><ElIconLocation /></el-icon>
+          <span>Navigator One</span>
+        </template>
+        <el-menu-item-group title="Group One">
+          <el-menu-item index="1-1">item one</el-menu-item>
+          <el-menu-item index="1-2">item two</el-menu-item>
+        </el-menu-item-group>
+      </el-sub-menu>
+      <el-menu-item index="2">
+        <el-icon><ElIconMenu /></el-icon>
+        <span>Navigator Two</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <el-icon><ElIconDocument /></el-icon>
+        <span>Navigator Three</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <el-icon><ElIconSetting /></el-icon>
+        <span>Navigator Four</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
 </template>
+
+<script lang="ts" setup>
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath);
+};
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath);
+};
+</script>
